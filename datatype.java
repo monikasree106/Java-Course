89a2ce57904002e9b06ea51cc7ffdd89cc585dcc
monikasree106 <monikasree.106@gmail.com>
2025-03-18 14:54:55 +0530

    Add files via upload
    Day 1
    
    Data Type & Variable

---
+++ datatype.java
@@ -0,0 +1,18 @@
+package program;
+
+public class datatype 
+{
+		public static void main(String[] args) 
+		{
+			int age = 18;
+			System.out.println(age);
+			char initial = 'D';
+			String name = "Monika";
+			System.out.println(initial);
+			System.out.println(name);
+			float f = 10.42006f;
+			double d = 18.10420062;
+			System.out.println(f);
+			System.out.println(d);
+		}
+}
---
+++ sample.java
@@ -0,0 +1,9 @@
+package program;
+
+public class sample {
+	public static void main(String[] args) {
+		System.out.println("welcome");
+		System.out.println("to");
+		System.out.println("java");
+	}
+}
