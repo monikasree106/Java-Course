503db8a70645bf899a0782b1b988d8dfda809330
monikasree106 <monikasree.106@gmail.com>
2025-03-18 11:41:32 +0530

    Add files via upload

---
+++ program/sam.java
@@ -0,0 +1,17 @@
+package program;
+
+public class sam 
+{
+	static public void main(String[]args)
+	{
+			int a = 5;
+			
+			System.out.println("c = "+(a++ + --a + ++a));
+		
+
+	}
+		
+}
+
+
+
---
+++ program/variable.java
@@ -0,0 +1,31 @@
+package program;
+
+public class variable
+{
+	int a =25; //instance variable
+	static int b = 50; //static variable
+	
+	public static int print()
+	{
+		int mark = 94;
+		return mark;
+	}
+	
+	void display()
+	{
+		int d =60; //local variable
+		System.out.println("method inside variable:" +d);
+	}
+	public static void main(String[] args)
+	{
+		int c = 75; //local variable
+		
+		variable obj1 = new variable();
+		System.out.println(obj1.a);
+		obj1.display();
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(print());
+		
+	}
+}
