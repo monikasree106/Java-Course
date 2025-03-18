65896a0d673f61f65e8a38a0df1ba1a22cbf79b1
monikasree106 <monikasree.106@gmail.com>
2025-03-18 14:51:50 +0530

    Delete program directory

--- program/sample.java
+++
@@ -1,9 +0,0 @@
-package program;
-
-public class sample {
-	public static void main(String[] args) {
-		System.out.println("welcome");
-		System.out.println("to");
-		System.out.println("java");
-	}
-}
